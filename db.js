865b8a735443ba26a92a51d88a278648773ae5e1
Alex <alex.saliu@gmail.com>
2021-01-03 14:43:01 +1030

    Committing to Heroku

--- db.js
+++ db.js
@@ -7,9 +7,10 @@ const {Pool, Client} = require('pg');
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_PORT = process.env.DB_PORT;
-const connectionString = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost:${DB_PORT}/saal`;
-const db = new Client({connectionString: connectionString});
-// const db = new Client("postgres://qsxqqkci:hWGkN9yANYqdvbHRAOU6bApsgpuS6_QN@rosie.db.elephantsql.com:5432/qsxqqkci");
+
+// const connectionString = `postgresql://${DB_USERNAME}:${DB_PASSWORD}@localhost:${DB_PORT}/saal`;
+const connectionString = `postgres://elucotbv:cbWLPWqArK8aTiaSrbuJ1M_87XUDnFlQ@rosie.db.elephantsql.com:5432/elucotbv`;
+const db = new Client({connectionString});
 
 db.connect();
 
--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const db = require('./db.js').dbConnection;
 const controller = require('./controller.js');
 const port = process.env.PORT;
 
-const { userJoin, getCurrentUser } = require('./helpers.js');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: true }));
 app.use(cors());
@@ -17,7 +15,29 @@ app.get('/', (req, res) => { res.send({json: "SAAL Backend API"}) })
 
 app.get('/data', async (req, res) => {
     const selectQuery = {
-        text: 'SELECT * FROM results',
+        text: 'SELECT * FROM results LIMIT 100',
+    }
+    db.query(selectQuery)
+        .then(data => {
+            res.status(200).json(data.rows);
+        })
+        .catch(err => {
+            res.status(400).send(err);
+        })
+})
+
+app.get('/athlete/:athlete', async (req, res) => {
+    const { athlete } = req.params
+    const selectQuery = {
+        text: `SELECT date, meetings.name AS meeting, distance, round, mark, time, adj_time, position FROM results
+    	JOIN athletes
+    	ON results.athlete_id = athletes.id
+    	JOIN events
+    	ON results.event_id = events.id
+    	JOIN meetings
+    	ON events.meeting_id = meetings.id
+    	WHERE athletes.name = $1`,
+        values: [athlete]
     }
     db.query(selectQuery)
         .then(data => {
