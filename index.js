e47a56e01f94e88183d2c3399b17a2a90e7e7ddc
Alex <alex.saliu@gmail.com>
2021-01-04 22:07:18 +1030

    More advanced querying and end points

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.options('*', cors());
 
 app.get('/', (req, res) => { res.send({json: "SAAL Backend API"}) })
 
-app.get('/data', async (req, res) => {
+app.get('/results', async (req, res) => {
     const selectQuery = {
         text: 'SELECT * FROM results LIMIT 100',
     }
@@ -26,22 +26,62 @@ app.get('/data', async (req, res) => {
         })
 })
 
-app.get('/athlete/:athlete', async (req, res) => {
-    const { athlete } = req.params
-    const selectQuery = {
-        text: `SELECT date, meetings.name AS meeting, distance, round, mark, time, adj_time, position FROM results
+app.get('/data', async (req, res) => {
+
+    const getTableRows = (table) => {
+        return {text: `SELECT * FROM ${table}`}
+    }
+
+    const athletes = await db.query(getTableRows('athletes'))
+    const meetings = await db.query(getTableRows('meetings'))
+    const events = await db.query(getTableRows('events'))
+
+    const data = {
+        athletes: athletes.rows,
+        meetings: meetings.rows,
+        events: events.rows
+    }
+
+    res.status(200).json(data)
+})
+
+app.get('/athlete', async (req, res) => {
+    const { athlete, distance, meeting, year } = req.query
+
+    const whereQuery = () => {
+        let q = ''
+        if (meeting && athlete || meeting && distance) q = 'AND '
+        if (athlete) {
+            q += `athletes.name = '${athlete}'`
+            if (distance) q += ' AND '
+        }
+        if (distance) q += `events.distance = '${distance}'`
+        return q
+    }
+
+    const selectQuery = `SELECT date, meetings.name AS meeting, distance, round, mark, time, adj_time, position, zscore, zscore_1u1d FROM results
     	JOIN athletes
     	ON results.athlete_id = athletes.id
     	JOIN events
     	ON results.event_id = events.id
     	JOIN meetings
-    	ON events.meeting_id = meetings.id
-    	WHERE athletes.name = $1`,
-        values: [athlete]
+    	ON events.meeting_id = meetings.id `
+
+    const Q1 = {
+        text: selectQuery + `WHERE meetings.name LIKE '%' || $1 || '%' ${whereQuery()}`,
+        values: [meeting]
     }
-    db.query(selectQuery)
+
+    const Q2 = {
+        text: selectQuery + `WHERE ${whereQuery()}`,
+    }
+
+    const Q = meeting ? Q1 : Q2
+
+    db.query(Q)
         .then(data => {
-            res.status(200).json(data.rows);
+            console.log(data);
+            res.status(200).send(data.rows);
         })
         .catch(err => {
             res.status(400).send(err);
